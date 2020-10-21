b602d1dcda1811abb089fea141c85779d324894e
Rob9315 <9315.rob@gmail.com>
2020-10-21 23:00:19 +0200

    .

--- index.js
+++ index.js
@@ -1,3 +1,47 @@
-module.exports.helloWorld = function () {
-  console.log('Hello world !')
+import mc from 'minecraft-protocol';
+import mineflayer from 'mineflayer';
+import { loadWelcomeMap } from './loadWelcomeMap.js';
+
+let badpackets = [];
+
+export class User {
+  constructor(){}
+  conn(proxyClient) {
+    switch (true) {
+      
+      default:
+        break;
+    }
+    this.proxyClient = proxyClient;
+		proxyClient.on('packet', (data, packetMeta) => {
+			console.log('PACKET', packetMeta.name, data);
+		});
+		proxyClient.on('end', (reason) => {
+      console.log('END', reason);
+      delete proxyClient
+		});
+		loadWelcomeMap(proxyClient);
+	}
+	static send({ data, packetMeta }, sender) {
+		if (!badpackets.includes(packetMeta.name)) {
+			sender.write(packetMeta.name, data);
+		}
+	}
 }
+
+export class BotConn{
+  constructor(host, port, username, password)
+}
+
+let users = [];
+
+let proxyServer = mc.createServer({
+	host: 'localhost',
+	port: 25566,
+	'online-mode': false,
+	motd: 'THE redirection tool mcproxy by Rob9315 (more or less)',
+});
+
+proxyServer.on('login', (proxyClient) => {
+	users.push(new User(proxyClient));
+});
