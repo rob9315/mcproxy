52121fa43b595944864e3925ae6ccd45164f18b7
Rob9315 <9315.rob@gmail.com>
2020-10-22 14:22:36 +0200

    typescript migration

--- index.js
+++ index.js
@@ -1,47 +1,99 @@
-import mc from 'minecraft-protocol';
-import mineflayer from 'mineflayer';
-import { loadWelcomeMap } from './loadWelcomeMap.js';
-
-let badpackets = [];
-
-export class User {
-  constructor(){}
-  conn(proxyClient) {
-    switch (true) {
-      
-      default:
-        break;
-    }
-    this.proxyClient = proxyClient;
-		proxyClient.on('packet', (data, packetMeta) => {
-			console.log('PACKET', packetMeta.name, data);
-		});
-		proxyClient.on('end', (reason) => {
-      console.log('END', reason);
-      delete proxyClient
-		});
-		loadWelcomeMap(proxyClient);
-	}
-	static send({ data, packetMeta }, sender) {
-		if (!badpackets.includes(packetMeta.name)) {
-			sender.write(packetMeta.name, data);
-		}
-	}
-}
-
-export class BotConn{
-  constructor(host, port, username, password)
-}
-
-let users = [];
-
-let proxyServer = mc.createServer({
-	host: 'localhost',
-	port: 25566,
-	'online-mode': false,
-	motd: 'THE redirection tool mcproxy by Rob9315 (more or less)',
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
 });
-
-proxyServer.on('login', (proxyClient) => {
-	users.push(new User(proxyClient));
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Proxy = exports.Room = exports.BotConn = exports.User = exports.Account = exports.Address = void 0;
+var mc = __importStar(require("minecraft-protocol"));
+var Address = /** @class */ (function () {
+    function Address(host, port) {
+        this.host = host;
+        this.port = port;
+    }
+    return Address;
+}());
+exports.Address = Address;
+var Account = /** @class */ (function () {
+    function Account(username, password) {
+        this.username = username;
+        this.password = password;
+    }
+    return Account;
+}());
+exports.Account = Account;
+var User = /** @class */ (function () {
+    function User(proxyClient) {
+        this.proxyClient = proxyClient;
+    }
+    return User;
+}());
+exports.User = User;
+var BotConn = /** @class */ (function () {
+    function BotConn(address, account) {
+        this.address = address;
+        this.account = account;
+    }
+    return BotConn;
+}());
+exports.BotConn = BotConn;
+var Room = /** @class */ (function () {
+    function Room(roomID, roomPassword) {
+        this.roomID = roomID;
+        this.roomPassword = roomPassword;
+        this.users = [];
+        this.botConns = [];
+    }
+    Room.prototype.addUser = function (user) {
+        if (this.roomPassword == null) {
+        }
+    };
+    Room.prototype.linkUser = function (user, botConn) { };
+    return Room;
+}());
+exports.Room = Room;
+var Proxy = /** @class */ (function () {
+    function Proxy(host, port, serverOptions) {
+        var _this = this;
+        this.host = host;
+        this.port = port;
+        this.serverOptions = serverOptions;
+        this.rooms = [];
+        this.rooms[0] = new Room(0, '');
+        this.proxyServer = mc.createServer(serverOptions);
+        this.users = [];
+        this.botConns = [];
+        this.proxyServer.on('login', function (proxyClient) {
+            var user = new User(proxyClient);
+            users.push(user);
+            _this.rooms[0].addUser(user);
+        });
+    }
+    return Proxy;
+}());
+exports.Proxy = Proxy;
+var users = [];
+var proxyServer = mc.createServer({
+    host: 'localhost',
+    port: 25566,
+    'online-mode': false,
+    motd: 'THE redirection tool mcproxy by Rob9315 (more or less)',
+});
+proxyServer.on('login', function (proxyClient) {
+    users.push(new User(proxyClient));
 });
---
+++ index.ts
@@ -0,0 +1,89 @@
+import * as mc from 'minecraft-protocol';
+import * as mineflayer from 'mineflayer';
+import * as loadWelcomeMap from './loadWelcomeMap';
+
+export class Address {
+	host: string;
+	port: number;
+	constructor(host: string, port: number) {
+		this.host = host;
+		this.port = port;
+	}
+}
+export class Account {
+	username: string;
+	password: string;
+	constructor(username: string, password: string) {
+		this.username = username;
+		this.password = password;
+	}
+}
+export class User {
+	proxyClient: mc.Client;
+	constructor(proxyClient: mc.Client) {
+		this.proxyClient = proxyClient;
+	}
+}
+export class BotConn {
+	address: Address;
+	account: Account;
+	constructor(address: Address, account: Account) {
+		this.address = address;
+		this.account = account;
+	}
+}
+export class Room {
+	users: User[];
+	botConns: BotConn[];
+	roomID: number;
+	roomPassword: string;
+	constructor(roomID: number, roomPassword: string) {
+		this.roomID = roomID;
+		this.roomPassword = roomPassword;
+		this.users = [];
+		this.botConns = [];
+	}
+	addUser(user: User) {
+		if (this.roomPassword == null) {
+		}
+	}
+	linkUser(user: User, botConn: BotConn) {}
+}
+export class Proxy {
+	rooms: Room[];
+	users: User[];
+	botConns: BotConn[];
+	host: string;
+	port: number;
+	proxyServer: mc.Server;
+	serverOptions: mc.ServerOptions;
+	constructor(host: string, port: number, serverOptions: mc.ServerOptions) {
+		this.host = host;
+		this.port = port;
+		this.serverOptions = serverOptions;
+		this.rooms = [];
+		this.rooms[0] = new Room(0, '');
+		this.proxyServer = mc.createServer(serverOptions);
+		this.users = [];
+		this.botConns = [];
+
+		this.proxyServer.on('login', (proxyClient) => {
+			let user = new User(proxyClient);
+			users.push(user);
+			this.rooms[0].addUser(user);
+		});
+	}
+}
+
+let users = [];
+
+let proxyServer = mc.createServer({
+	host: 'localhost',
+	port: 25566,
+	'online-mode': false,
+	motd: 'THE redirection tool mcproxy by Rob9315 (more or less)',
+});
+
+proxyServer.on('login', (proxyClient) => {
+	users.push(new User(proxyClient));
+});
