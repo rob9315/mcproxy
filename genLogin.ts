d0736f65fa4e3c992b0139091fee5d9c791d054f
Rob9315 <9315.rob@gmail.com>
2020-10-23 13:32:07 +0200

    map_chunk implementation

--- genLogin.ts
+++ genLogin.ts
@@ -3,37 +3,111 @@ import mc from 'minecraft-protocol';
 import mcdata from 'minecraft-data';
 
 export function genLogin(bot: mineflayer.Bot, client: mc.Client) {
-	client.write('success', { uuid: bot._client.uuid, username: bot.username });
-	((bot.entities as unknown) as mcdata.Entities).forEach((entity) => {
-		switch (true) {
-			case hP(entity, [
-				'id',
-				'uuid',
-				'type',
-				'position',
-				'pitch',
-				'yaw',
-				'metadata',
-				'velocity',
-			]):
-				client.write('spawn_entity', {
-					entityId: entity.id,
-					objectUUID: ((entity as unknown) as any).uuid,
-				});
-				break;
+  send(
+    {
+      data: {
+        entityId: ((bot.entity as unknown) as mcdata.Entity).id,
+        gameMode: bot.player.gamemode,
+        dimension: bot.game.dimension,
+        difficulty: bot.game.difficulty,
+        maxPlayers: bot.game.maxPlayers,
+        levelType: bot.game.levelType,
+        reducedDebugInfo: false,
+      },
+      meta: { name: 'login' },
+    },
+    client,
+  );
 
-			default:
-				break;
-		}
-	});
+  let columnArray = ((bot as unknown) as any).world.getColumns();
+
+  for (const index in columnArray) {
+    if (Object.prototype.hasOwnProperty.call(columnArray, index)) {
+      const chunk = columnArray[index];
+      send(
+        buildChunkPacket({
+          x: chunk.chunkX,
+          z: chunk.chunkZ,
+          chunk: chunk.column,
+        }),
+        client,
+      );
+    }
+  }
+
+  ((bot as unknown) as any).world.getColumns();
+
+  bot._client.on('packet', (data, packetMeta) => {
+    if (packetMeta.name != 'keep_alive') {
+      client.write(packetMeta.name, data);
+    }
+  });
+  client.on('packet', (data, packetMeta) => {
+    if (packetMeta.name != 'keep_alive') {
+      bot._client.write(packetMeta.name, data);
+    }
+  });
+  //   ((bot.entities as unknown) as mcdata.Entities).forEach((entity) => {
+  //     switch (true) {
+  //       case hP(entity, [
+  //         'id',
+  //         'uuid',
+  //         'type',
+  //         'position',
+  //         'pitch',
+  //         'yaw',
+  //         'metadata',
+  //         'velocity',
+  //       ]):
+  //         client.write('spawn_entity', {
+  //           entityId: entity.id,
+  //           objectUUID: ((entity as unknown) as any).uuid,
+  //         });
+  //         break;
+
+  //       default:
+  //         break;
+  //     }
+  //   });
+}
+
+function buildChunkPacket({
+  x,
+  z,
+  chunk,
+}: {
+  x: string;
+  z: string;
+  chunk: any;
+}) {
+  var meta = { name: 'map_chunk' };
+  var data = {
+    x: x,
+    z: z,
+    groundUp: true,
+    bitMap: chunk.getMask(),
+    chunkData: chunk.dump(),
+    blockEntities: [],
+  };
+  return { data, meta };
+}
+
+function send({ data, meta }: any, sender: mc.Client) {
+  if (!['keep_alive'].includes(meta.name)) {
+    sender.write(meta.name, data);
+    console.log(meta.name, data);
+  }
 }
 
 //* hP stands for hasProperty
 function hP(object: any, keys: string[]) {
-	keys.forEach((key) => {
-		if (Object.prototype.hasOwnProperty.call(object, key)) {
-			return false;
-		}
-	});
-	return true;
+  keys.forEach((key) => {
+    if (Object.prototype.hasOwnProperty.call(object, key)) {
+      return false;
+    }
+  });
+  return true;
 }
+
+//! SENDING A MESSAGE
+// proxyClient.write("chat", {message: '{"translate":"chat.type.announcement","with":[{"text":"Server"},{"extra":[{"text":"hi"}],"text":""}]}'})
--- index.ts
+++ index.ts
@@ -3,113 +3,114 @@ import mineflayer from 'mineflayer';
 import { loadWelcomeMap } from './loadWelcomeMap.js';
 
 export class Address {
-	host: string;
-	port: number;
-	constructor(host: string, port: number) {
-		this.host = host;
-		this.port = port;
-	}
+  host: string;
+  port: number;
+  constructor(host: string, port: number) {
+    this.host = host;
+    this.port = port;
+  }
 }
 export class Account {
-	username: string;
-	password: string;
-	constructor(username: string, password: string) {
-		this.username = username;
-		this.password = password;
-	}
+  username: string;
+  password: string;
+  constructor(username: string, password: string) {
+    this.username = username;
+    this.password = password;
+  }
 }
 export class User {
-	proxyClient: mc.Client;
-	constructor(proxyClient: mc.Client) {
-		this.proxyClient = proxyClient;
-		loadWelcomeMap(this.proxyClient);
-		this.proxyClient.on('packet', (data, packetMeta) => {
-			output(this, data, packetMeta);
-		});
-	}
+  proxyClient: mc.Client;
+  constructor(proxyClient: mc.Client) {
+    this.proxyClient = proxyClient;
+    loadWelcomeMap(this.proxyClient);
+    this.proxyClient.on('packet', (data, packetMeta) => {
+      output(this, data, packetMeta);
+    });
+  }
 }
 export class BotConn {
-	address: Address;
-	account: Account;
-	bot: mineflayer.Bot;
-	client: mc.Client;
-	constructor(address: Address, account: Account) {
-		this.address = address;
-		this.account = account;
-		this.bot = mineflayer.createBot({
-			username: account.username,
-			password: account.password,
-			host: address.host,
-			port: address.port,
-		});
-		this.client = this.bot._client;
-	}
+  address: Address;
+  account: Account;
+  bot: mineflayer.Bot;
+  client: mc.Client;
+  constructor(address: Address, account: Account) {
+    this.address = address;
+    this.account = account;
+    this.bot = mineflayer.createBot({
+      username: account.username,
+      password: account.password,
+      host: address.host,
+      port: address.port,
+    });
+    this.client = this.bot._client;
+  }
 }
 export class Room {
-	users: User[];
-	botConns: BotConn[];
-	roomID: number;
-	roomPassword: string;
-	constructor(roomID: number, roomPassword: string) {
-		this.roomID = roomID;
-		this.roomPassword = roomPassword;
-		this.users = [];
-		this.botConns = [];
-	}
-	addUser(user: User) {
-		if (!(this.roomPassword == null)) {
-		} else {
-			this.users.push(user);
-		}
-	}
-	addBotConn(botConn: BotConn) {
-		this.botConns.push(botConn);
-	}
-	linkUser(user: User, botConn: BotConn) {}
+  users: User[];
+  botConns: BotConn[];
+  roomID: number;
+  roomPassword: string;
+  constructor(roomID: number, roomPassword: string) {
+    this.roomID = roomID;
+    this.roomPassword = roomPassword;
+    this.users = [];
+    this.botConns = [];
+  }
+  addUser(user: User) {
+    if (!(this.roomPassword == null)) {
+    } else {
+      this.users.push(user);
+    }
+  }
+  addBotConn(botConn: BotConn) {
+    this.botConns.push(botConn);
+  }
+  linkUser(user: User, botConn: BotConn) {}
 }
 export class Proxy {
-	rooms: Room[];
-	users: User[];
-	botConns: BotConn[];
-	host: string;
-	port: number;
-	proxyServer: mc.Server;
-	serverOptions: mc.ServerOptions;
-	constructor(host: string, port: number) {
-		this.host = host;
-		this.port = port;
-		this.serverOptions = {
-			host: host,
-			port: port,
-			'online-mode': false,
-			motd: 'A redirection tool, mcproxy by Rob9315',
-		};
-		this.rooms = [];
-		this.rooms[0] = new Room(0, '');
-		this.proxyServer = mc.createServer(this.serverOptions);
-		this.users = [];
-		this.botConns = [];
+  rooms: Room[];
+  users: User[];
+  botConns: BotConn[];
+  host: string;
+  port: number;
+  proxyServer: mc.Server;
+  serverOptions: mc.ServerOptions;
+  constructor(host: string, port: number) {
+    this.host = host;
+    this.port = port;
+    this.serverOptions = {
+      host: host,
+      port: port,
+      'online-mode': false,
+      motd: 'A redirection tool, mcproxy by Rob9315',
+    };
+    this.rooms = [];
+    this.rooms[0] = new Room(0, '');
+    this.proxyServer = mc.createServer(this.serverOptions);
+    this.users = [];
+    this.botConns = [];
 
-		this.proxyServer.on('login', (proxyClient) => {
-			let user = new User(proxyClient);
-			this.users.push(user);
-			this.rooms[0].addUser(user);
-		});
-	}
+    this.proxyServer.on('login', (proxyClient) => {
+      let user = new User(proxyClient);
+      this.users.push(user);
+      this.rooms[0].addUser(user);
+    });
+  }
 }
 
 export function output(object: any, data: any, packetMeta: mc.PacketMeta) {
-	if (!(packetMeta.name == 'keep_alive')) {
-		if (object.name) {
-			console.log(object.name, packetMeta.state, packetMeta.name, data);
-		} else {
-			console.log(typeof object, packetMeta.state, packetMeta.name, data);
-		}
-	}
-	if (
-		packetMeta.name == 'chat' &&
-		(data.message as string).includes('debugpls')
-	) {
-		console.log();
-	}
+  //!debug
+  if (!(packetMeta.name == 'keep_alive') && false) {
+    if (object.name) {
+      console.log(object.name, packetMeta.state, packetMeta.name, data);
+    } else {
+      console.log(typeof object, packetMeta.state, packetMeta.name, data);
+    }
+  }
+  if (
+    packetMeta.name == 'chat' &&
+    (data.message as string).includes('debugpls')
+  ) {
+    console.log();
+  }
 }
